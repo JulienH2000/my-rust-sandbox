6111147cae7ebd2d5298977be200e1227c9220f5
JulienH2000 <julien.hugues@outlook.com>
2023-12-08 19:20:51 +0100

    init commit

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use std::io;
+
+fn main() {
+
+    let retour = text_return();
+    let modif = text_modif(retour.clone());
+    let mut user_input = String::new();
+
+    println!("{}", retour);
+    println!("{}", modif);
+
+    println!("saisie i32: ");
+
+
+    let user_input : i32 = loop {
+        match io::stdin()
+            .read_line(&mut user_input)
+            .expect("error")
+            .trim()
+            .parse() {
+            Ok(int) => int,
+            Err(_) => continue,
+        };
+    };
+
+
+    println!("{}", nb_push(modif, user_input));
+
+}
+
+fn text_return() -> String {
+
+    let texte = String::from("texte");
+
+    texte
+}
+
+fn text_modif(input: String) -> String {
+
+    let mut modif = input;
+    modif.push_str("_modif");
+
+    modif
+
+}
+
+fn nb_push(mut text_input : String, nb_input : i32) -> String {
+
+    let tmp : String = nb_input.to_string();
+    text_input.push_str(&tmp);
+
+    text_input
+}
